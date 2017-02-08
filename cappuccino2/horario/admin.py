29cf25eff7aba07c1d9c9dc7538b18179b5e896b
Daniel Saguez Tezanos Pinto <danielstp@gmail.com>
2017-02-07 23:23:37 -0400

    corregi dia semana y requerimientos

--- cappuccino2/horario/admin.py
+++ cappuccino2/horario/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Carrera
+from .models import Docente
+from .models import Ayudante
+from .models import Materia
+from .models import Aula
+from .models import Grupo
+from .models import Horario
+
+admin.site.register(Carrera)
+admin.site.register(Docente)
+admin.site.register(Ayudante)
+admin.site.register(Materia)
+admin.site.register(Aula)
+admin.site.register(Grupo)
+admin.site.register(Horario)
