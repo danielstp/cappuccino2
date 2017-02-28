890e1421ad2aa2e801f77d1910bb687dfe6dc656
Daniel Saguez Tezanos Pinto <danielstp@gmail.com>
2017-02-28 11:16:15 -0400

    REST + mejora proceso horarios url corregidas despues del rebase

--- cappuccino2/horario/models.py
+++ cappuccino2/horario/models.py
@@ -10,52 +10,74 @@ DÍA_SEMANA = (
     ('DO', 'DO'),
 )
 
+
 class Carrera(models.Model):
     codigo = models.IntegerField(primary_key=True)
     nombre = models.CharField(max_length=255)
     pdf = models.URLField()
     fecha = models.DateTimeField(default='2016-01-01 00:00:00-04')
     fechaPDF = models.DateTimeField(default='2016-01-01 00:00:00-04')
+
     def __str__(self):
         return self.nombre
+
+class Actualización(models.Model):
+    carrera = models.ForeignKey(Carrera)
+    fecha = models.DateTimeField(default='2016-01-01 00:00:00-04')
+    fechaPDF = models.DateTimeField(default='2016-01-01 00:00:00-04')
+
+    def __str__(self):
+        return self.carrera.nombre + self.fecha
+
 
 class Docente(models.Model):
     nombre = models.CharField(max_length=255)
+
     def __str__(self):
         return self.nombre
 
+
 class Ayudante(models.Model):
     nombre = models.CharField(max_length=255)
+
     def __str__(self):
         return self.nombre
+
 
 class Materia(models.Model):
     nombre = models.CharField(max_length=255)
     codigo = models.IntegerField(primary_key=True)
+    nivel = models.CharField(max_length=1)
+
     def __str__(self):
         return self.nombre
 
+
 class Aula(models.Model):
-    codigo = models.CharField(max_length=25,primary_key=True)
+    codigo = models.CharField(max_length=25, primary_key=True)
+
     def __str__(self):
         return self.codigo
 
 
 class Grupo(models.Model):
-    codigo = models.CharField(max_length=25,primary_key=True)
+    codigo = models.CharField(max_length=25, primary_key=True)
+    grupo = models.CharField(max_length=2)
     materia = models.ForeignKey(Materia)
-    docente = models.ForeignKey(Docente,null=True)
-    ayudante = models.ForeignKey(Ayudante,null=True)
+    docente = models.ForeignKey(Docente, null=True)
+    ayudante = models.ForeignKey(Ayudante, null=True)
+
     def __str__(self):
-        return self.materia.nombre+' Grupo:'+self.codigo
+        return self.materia.nombre + ' Grupo:' + self.codigo
 
 
 class Horario(models.Model):
-    codigo = models.CharField(max_length=25,primary_key=True)
-    día = models.CharField(max_length=2,choices=DÍA_SEMANA)
+    codigo = models.CharField(max_length=25, primary_key=True)
+    día = models.CharField(max_length=2, choices=DÍA_SEMANA)
     inicio = models.TimeField()
     fin = models.TimeField()
     aula = models.ForeignKey(Aula)
     grupo = models.ForeignKey(Grupo)
+
     def __str__(self):
-        return self.grupo.materia.nombre+' Grupo:'+ self.codigo
+        return self.grupo.materia.nombre + ' Grupo:' + self.codigo
