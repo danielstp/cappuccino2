da7dd4b2cb2b7c8e3fb6a19010400541473064c4
Daniel Saguez Tezanos Pinto <daniel.saguez@salamancasolutions.com>
2019-02-18 19:25:42 -0400

    Actualizado a Django 2.1

--- cappuccino2/horario/urls.py
+++ cappuccino2/horario/urls.py
@@ -14,25 +14,9 @@ router.register(r'nivel', views.NivelMateriaViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(
         regex=r'^$',
         view=views.HorarioListView.as_view(),
         name='list'
     ),
-    #    url(
-    #        regex=r'^~redirect/$',
-    #        view=views.UserRedirectView.as_view(),
-    #        name='redirect'
-    #    ),
-    #    url(
-    #        regex=r'^(?P<username>[\w.@+-]+)/$',
-    #        view=views.UserDetailView.as_view(),
-    #        name='detail'
-    #    ),
-    #    url(
-    #        regex=r'^~update/$',
-    #        view=views.UserUpdateView.as_view(),
-    #        name='update'
-    #    ),
 ]
--- cappuccino2/users/views.py
+++ cappuccino2/users/views.py
@@ -1,48 +1,26 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, unicode_literals
-
-from django.core.urlresolvers import reverse
-from django.views.generic import DetailView, ListView, RedirectView, UpdateView
-
-from django.contrib.auth.mixins import LoginRequiredMixin
-
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import AllowAny
 from .models import User
+from .permissions import IsUserOrReadOnly
+from .serializers import CreateUserSerializer, UserSerializer
 
 
-class UserDetailView(LoginRequiredMixin, DetailView):
-    model = User
-    # These next two lines tell the view to index lookups by username
-    slug_field = 'username'
-    slug_url_kwarg = 'username'
+class UserViewSet(mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  viewsets.GenericViewSet):
+    """
+    Updates and retrieves user accounts
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsUserOrReadOnly,)
 
 
-class UserRedirectView(LoginRequiredMixin, RedirectView):
-    permanent = False
-
-    def get_redirect_url(self):
-        return reverse('users:detail',
-                       kwargs={'username': self.request.user.username})
-
-
-class UserUpdateView(LoginRequiredMixin, UpdateView):
-
-    fields = ['name', ]
-
-    # we already imported User in the view code above, remember?
-    model = User
-
-    # send the user back to their own page after a successful update
-    def get_success_url(self):
-        return reverse('users:detail',
-                       kwargs={'username': self.request.user.username})
-
-    def get_object(self):
-        # Only get the User record for the user making the request
-        return User.objects.get(username=self.request.user.username)
-
-
-class UserListView(LoginRequiredMixin, ListView):
-    model = User
-    # These next two lines tell the view to index lookups by username
-    slug_field = 'username'
-    slug_url_kwarg = 'username'
+class UserCreateViewSet(mixins.CreateModelMixin,
+                        viewsets.GenericViewSet):
+    """
+    Creates user accounts
+    """
+    queryset = User.objects.all()
+    serializer_class = CreateUserSerializer
+    permission_classes = (AllowAny,)
