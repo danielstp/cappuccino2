e15b7d23c47375b469ee23e96309c54eb890c0e4
Daniel Saguez Tezanos Pinto <daniel.saguez@salamancasolutions.com>
2019-02-22 17:42:59 -0400

    filtros

--- cappuccino2/horario/views.py
+++ cappuccino2/horario/views.py
@@ -2,24 +2,30 @@ from django.shortcuts import render
 from django.views.generic import DetailView, ListView, RedirectView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from cappuccino2.horario.models import Carrera, NivelMateria, Materia
+from cappuccino2.horario.models import ( Carrera, NivelMateria, Materia,
+    Actualización, Docente, Ayudante, Aula, Grupo, Horario )
 #from django.contrib.auth.models import Group
 
 from rest_framework import viewsets
-from cappuccino2.horario.serializers import CarreraSerializer, NivelMateriaSerializer, MateriaSerializer
-
+from cappuccino2.horario.serializers import ( CarreraSerializer,
+    NivelMateriaSerializer, MateriaSerializer, ActualizaciónSerializer,
+    DocenteSerializer, AyudanteSerializer, AulaSerializer, GrupoSerializer,
+    HorarioSerializer)
 
 class CarreraViewSet(viewsets.ModelViewSet):
     """
-    API endpoint 
+    API endpoint
     """
-    queryset = Carrera.objects.all().order_by('nombre')
+    queryset = Carrera.objects.all()
     serializer_class = CarreraSerializer
+    filter_fields  = ('código','nombre','pdf','fecha','fechaPDF',)
+
+
 
 
 class MateriaViewSet(viewsets.ModelViewSet):
     """
-    API endpoint 
+    API endpoint
     """
     queryset = Materia.objects.all()
     serializer_class = MateriaSerializer
@@ -38,3 +44,53 @@ class HorarioListView(LoginRequiredMixin, ListView):
     # These next two lines tell the view to index lookups by username
     slug_field = 'username'
     slug_url_kwarg = 'username'
+
+
+
+class ActualizaciónViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint
+    """
+    queryset = Actualización.objects.all()
+    serializer_class = ActualizaciónSerializer
+
+
+class DocenteViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint
+    """
+    queryset = Docente.objects.all()
+    serializer_class = DocenteSerializer
+
+
+class AyudanteViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint
+    """
+    queryset = Ayudante.objects.all()
+    serializer_class = AyudanteSerializer
+
+
+class AulaViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint
+    """
+    queryset = Aula.objects.all()
+    serializer_class = AulaSerializer
+
+
+class GrupoViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint
+    """
+    queryset = Grupo.objects.all()
+    serializer_class = GrupoSerializer
+
+
+class HorarioViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint
+    """
+    queryset = Horario.objects.all()
+    serializer_class = HorarioSerializer
+    filter_fields  = ('código','día','inicio','fin','aula','grupo','grupo__materia')
