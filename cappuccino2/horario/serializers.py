dd41b8efaa0ff72fe08ad22daa9cf8677ffcc5ba
Daniel Saguez Tezanos Pinto <daniel.saguez@salamancasolutions.com>
2019-02-25 20:56:30 -0400

    extensions + router mejorado

--- cappuccino2/horario/serializers.py
+++ cappuccino2/horario/serializers.py
@@ -5,59 +5,61 @@ from cappuccino2.horario.models import ( Carrera, NivelMateria, Materia,
 from rest_framework import serializers
 
 
-class CarreraSerializer(serializers.HyperlinkedModelSerializer):
+class CarreraSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Carrera
         fields = ('__all__')
 
 
-class MateriaSerializer(serializers.HyperlinkedModelSerializer):
+class MateriaSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Materia
         fields = ('__all__')
 
 
-class NivelMateriaSerializer(serializers.HyperlinkedModelSerializer):
+class NivelMateriaSerializer(serializers.ModelSerializer):
+
+    materiaNombre = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         model = NivelMateria
-        fields = ('__all__')
+        fields = ('materia', 'carrera', 'nivel','materiaNombre')
 
 
-class ActualizaciónSerializer(serializers.HyperlinkedModelSerializer):
+class ActualizaciónSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Actualización
         fields = ('__all__')
 
-class DocenteSerializer(serializers.HyperlinkedModelSerializer):
+class DocenteSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Docente
         fields = ('__all__')
 
-class AyudanteSerializer(serializers.HyperlinkedModelSerializer):
+class AyudanteSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Ayudante
         fields = ('__all__')
 
-class AulaSerializer(serializers.HyperlinkedModelSerializer):
+class AulaSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Aula
         fields = ('__all__')
 
 
-class GrupoSerializer(serializers.HyperlinkedModelSerializer):
+class GrupoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Grupo
         fields = ('__all__')
 
-class HorarioSerializer(serializers.HyperlinkedModelSerializer):
+class HorarioSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Horario
