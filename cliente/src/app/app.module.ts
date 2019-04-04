7d6e7a441e24286d5934d6fc98662cb43bae18a2
Daniel Saguez Tezanos Pinto <daniel.saguez@salamancasolutions.com>
2019-04-03 22:06:32 -0400

    Inicio de frontend

---
+++ cliente/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MateriasComponent } from './materias/materias.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MateriasComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
