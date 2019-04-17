9441debd98442ec44458d0eea8e74d10a5e476e5
Daniel Saguez Tezanos Pinto <daniel.saguez@salamancasolutions.com>
2019-04-17 12:40:34 -0400

    Horario

--- cliente/src/app/app.module.ts
+++ cliente/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MateriasComponent } from './materias/materias.component';
+import { HorarioComponent } from './horario/horario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MateriasComponent
+    MateriasComponent,
+    HorarioComponent
   ],
   imports: [
     BrowserModule,
--- cliente/src/app/materia.service.ts
+++ cliente/src/app/materia.service.ts
@@ -20,5 +20,4 @@ export class MateriaService {
 
   constructor(private http: HttpClient) { }
 
-
 }
