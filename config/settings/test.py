9ad44b954856a9da9c3e39a71d448f25aa28cccb
Daniel Saguez Tezanos Pinto <daniel.saguez@bairesdev.com>
2021-10-11 02:22:14 -0400

    Updated to Django 3.1.13 {Rebase 1}

--- config/settings/test.py
+++ config/settings/test.py
@@ -21,6 +21,7 @@ TEST_RUNNER = "django.test.runner.DiscoverRunner"
 # https://docs.djangoproject.com/en/dev/ref/settings/#password-hashers
 PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
 
+
 # EMAIL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
