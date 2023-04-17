742cc51b479b81eda61dfe1848333ee4603be9c9
Daniel Saguez Tezanos Pinto <danielstp@gmail.com>
2023-04-17 13:47:32 -0400

    cookiecutter-django updated

--- cappuccino2/horario/urls.py
+++ cappuccino2/horario/urls.py
@@ -1,20 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-from django.conf.urls import url, include
-from rest_framework import routers
-
-from . import views
 from .. import urls as urlsRoot
+from . import views
 
 router = urlsRoot.router
-router.register(r'carreras', views.CarreraViewSet)
-router.register(r'materias', views.MateriaViewSet)
-router.register(r'nivel', views.NivelMateriaViewSet)
-router.register(r'actualización', views.ActualizaciónViewSet)
-router.register(r'docente', views.DocenteViewSet)
-router.register(r'ayudante', views.AyudanteViewSet)
-router.register(r'aula', views.AulaViewSet)
-router.register(r'grupo', views.GrupoViewSet)
-router.register(r'horario', views.HorarioViewSet)
-
+router.register(r"carreras", views.CarreraViewSet)
+router.register(r"materias", views.MateriaViewSet)
+router.register(r"nivel", views.NivelMateriaViewSet)
+router.register(r"actualización", views.ActualizaciónViewSet)
+router.register(r"docente", views.DocenteViewSet)
+router.register(r"ayudante", views.AyudanteViewSet)
+router.register(r"aula", views.AulaViewSet)
+router.register(r"grupo", views.GrupoViewSet)
+router.register(r"horario", views.HorarioViewSet)
--- config/settings/base.py
+++ config/settings/base.py
@@ -78,6 +78,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "cappuccino2.users",
+    "cappuccino2.horario",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
