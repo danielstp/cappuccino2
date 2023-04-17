a85d25fd63f6b580af1124bb2cce570319797763
Daniel Saguez Tezanos Pinto <danielstp@gmail.com>
2023-04-17 13:47:32 -0400

    cookiecutter-django updated {Rebase 1}

--- cappuccino2/horario/apps.py
+++ cappuccino2/horario/apps.py
@@ -2,9 +2,9 @@ from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
 
-class UsersConfig(AppConfig):
-    name = "cappuccino2.users"
-    verbose_name = _("Users")
+class HorarioConfig(AppConfig):
+    name = "cappuccino2.horario"
+    verbose_name = _("Horario")
 
     def ready(self):
         try:
