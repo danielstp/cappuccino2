98286c18b28e44ffeba877b4fd0761ab47c47c1f
Daniel Saguez Tezanos Pinto <danielstp@gmail.com>
2025-08-26 22:15:42 -0400

    Mejorar Admin {rebase}

--- cappuccino2/horario/admin.py
+++ cappuccino2/horario/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 
-from .models import Aula, Ayudante, Carrera, Docente, Grupo, Horario, Materia
+from .models import Actualización, Aula, Ayudante, Carrera, Docente, Grupo, Horario, Materia, NivelMateria
 
+admin.site.register(Actualización)
 admin.site.register(Carrera)
 admin.site.register(Docente)
 admin.site.register(Ayudante)
 admin.site.register(Materia)
+admin.site.register(NivelMateria)
 admin.site.register(Aula)
 admin.site.register(Grupo)
 admin.site.register(Horario)
